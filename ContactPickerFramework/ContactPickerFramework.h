4a030e215f1e66b6083970fd509da5d4710e4407
Nguyen Dung <dungnt@vivas.vn>
2016-12-06 15:36:21 +0700

    *

---
+++ ContactPickerFramework/ContactPickerFramework.h
@@ -0,0 +1,24 @@
+//
+//  ContactPickerFramework.h
+//  ContactPickerFramework
+//
+//  Created by Nguyen Dung on 12/6/16.
+//  Copyright © 2016 Tristan Himmelman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ContactPickerFramework.
+FOUNDATION_EXPORT double ContactPickerFrameworkVersionNumber;
+
+//! Project version string for ContactPickerFramework.
+FOUNDATION_EXPORT const unsigned char ContactPickerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ContactPickerFramework/PublicHeader.h>
+#import <ContactPickerFramework/THContactViewStyle.h>
+#import <ContactPickerFramework/THContactView.h>
+#import <ContactPickerFramework/THContactPickerView.h>
+#import <ContactPickerFramework/THContactTextField.h>
+
+
+
